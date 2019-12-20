24daf11785a71adda95d833e957ae1a07c331407
Yaroslav Polyakov <yaroslaff@gmail.com>
2019-12-21 05:27:49 +0700

    version 0.1.15 now check each DocumentRoot in LetsEncrypt config (to catch problem with typos there or other problems where only part of domain names passes checks)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='a2conf',
-    version='0.1.14',
+    version='0.1.15',
     packages=['a2conf'],
     scripts=[ 'bin/a2conf', 'bin/apache2okerr.py', 'bin/apache2-certbot-diag.py' ],
 
